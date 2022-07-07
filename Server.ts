17d8cbd587c93ca58a0168a864715bd6d98abb1f
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-07 15:45:36 +0200

    fix

--- Server.ts
+++ Server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import { connectToDatabase, connectToPort } from './src/functions'
-import { errorHandler } from './src/middlewares/errorHandler'
+import { errorHandler, notFound } from './src/middlewares'
 
 const application = express()
 application.use(express.json())
 application.use(morgan('common'))
+
+application.use(notFound)
+application.use(errorHandler)
 
 connectToDatabase()
 connectToPort(application)
