170fa051bbfafe008aaf50af6de04c365a2bb309
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-07 15:55:04 +0200

    added exports

--- src/app/controllers/User.controller.ts
+++ src/app/controllers/User.controller.ts
@@ -2,15 +2,15 @@ import StatusCode from "../../configurations/StatusCode"
 import UserModel from "../models/User.model"
 
 const createUser = async (request, response) => {
-	const user = new UserModel({
-		walletID: request.body.walletID,
-	})
-	try {
-		const databaseResponse = await user.save()
-		response.status(StatusCode.CREATED).send(databaseResponse)
-	} catch (error) {
-		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({ message: error.message })
-	}
+  const user = new UserModel({
+    walletID: request.body.walletID,
+  })
+  try {
+    const databaseResponse = await user.save()
+    response.status(StatusCode.CREATED).send(databaseResponse)
+  } catch (error) {
+    response.status(StatusCode.INTERNAL_SERVER_ERROR).send({ message: error.message })
+  }
 }
 
 const login = () => {
@@ -19,4 +19,10 @@ const login = () => {
 
 const getAllUsers = () => {
 
+}
+
+export default {
+  createUser,
+  login,
+  getAllUsers,
 }
