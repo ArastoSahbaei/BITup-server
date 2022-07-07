7f599dbe162ca13a66d78ea353b0ef924d44f381
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-07 15:11:09 +0200

    model update

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true,
+    unique: true
+  },
+}, { timestamps: true })
+
+const UserModel = mongoose.model('user', userSchema)
+export default UserModel
