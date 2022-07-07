eb92fe0f189c2774f832626f3d78cfd6e97ffedf
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-07 15:59:20 +0200

    added routes

--- Server.ts
+++ Server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
+import UserRoute from './src/app/routes/User.route'
 import { connectToDatabase, connectToPort } from './src/functions'
 import { errorHandler, notFound } from './src/middlewares'
 
 const application = express()
 application.use(express.json())
 application.use(morgan('common'))
+
+UserRoute.routes(application)
 
 application.use(notFound)
 application.use(errorHandler)
---
+++ src/app/routes/User.route.ts
@@ -0,0 +1,10 @@
+import { Application } from 'express'
+import UserController from '../controllers/User.controller'
+
+const routes = (application: Application) => {
+	application.post('/register', UserController.createUser)
+	application.post('/login', UserController.login)
+	application.get('/user', UserController.getAllUsers)
+}
+
+export default { routes }
