397f3f9e5bad150c3c114718632de0e6ce8dcedb
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-19 10:13:56 +0200

    fixed cors

--- Server.ts
+++ Server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import UserRoute from './src/app/routes/User.route'
 import { connectToDatabase, connectToPort } from './src/functions'
 import { errorHandler, notFound } from './src/middlewares'
 
 const application = express()
+application.use(cors({ credentials: true }))
 application.use(express.json())
 application.use(morgan('common'))
 
