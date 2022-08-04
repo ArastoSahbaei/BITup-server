d7ef901f3d4b07cf4785330f3a0a7c1e96e11d92
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-08-04 15:51:35 +0200

    m

--- src/app/routes/BTCPay.route.ts
+++ src/app/routes/BTCPay.route.ts
@@ -2,7 +2,7 @@ import { Application } from 'express'
 import BTCPayController from '../controllers/BTCPay.controller'
 
 const routes = (application: Application) => {
-	/* application.post('/btcpay/invoices', BTCPayController.getInvoices) */
+	application.post('/btcpay/invoices', BTCPayController.getInvoices)
 	application.post('/btcpay/invoices/create', BTCPayController.createInvoice)
 }
 
