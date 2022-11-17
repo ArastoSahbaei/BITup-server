e70a9129b7c8310e127b026894efb909639c4ac4
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-11-17 16:52:29 +0100

    working

--- src/shared/api/services/BinanceService.ts
+++ src/shared/api/services/BinanceService.ts
@@ -1,22 +1,19 @@
 import http from '../BinanceAPI'
 import CryptoJS from 'crypto-js'
 
-const endPoint = '/api/v3/ping'
-const endPoint2 = '/api/v3/account'
+
 const dataQueryString = 'recvWindow=20000&timestamp=' + Date.now()
-const secret = '62118747723bd91218d516a782214f0131f53e811cbdaf248dbe3055d22f3016'
-
+const secret = 'zzhHWaOeQSNM18pzHwHtVXNgjZ0pqDiGOSbuN274cOTSQrDCBXJWh7PwIGa8Zzb5'
 const signature = CryptoJS.HmacSHA256(dataQueryString, secret).toString(CryptoJS.enc.Hex)
-
-const url = endPoint + '?' + dataQueryString + '&signature=' + signature
+const url = '/api/v3/account?' + dataQueryString + '&signature=' + signature
 
 
 const test = () => {
-	return http.get(endPoint)
+	return http.get('/api/v3/ping')
 }
 
 const getAccountInformation = () => {
-	return http.get(url)
+	return http.get('/api/v3/account?' + dataQueryString + '&signature=' + signature)
 }
 
 const createSellOrder = () => {
