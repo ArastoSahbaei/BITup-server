10323e531a6a3818e2b62068efde4c535f264347
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-17 22:13:23 +0100

    lez go

--- src/middlewares/authenticateWebHook.ts
+++ src/middlewares/authenticateWebHook.ts
@@ -1,10 +1,10 @@
 import crypto from 'crypto'
 import StatusCode from '../configurations/StatusCode'
 
-const secret = 'a4YthnHjwYJ8qjEzgA2w7pouq1B' // The secret key provided by BTCPay Server
 export const authenticateWebHook = (request, response, next) => {
 	console.log('wtf is going on?')
-
+	console.log(request.body)
+	const secret = 'a4YthnHjwYJ8qjEzgA2w7pouq1B' // The secret key provided by BTCPay Server
 	// Extract the signature from the headers
 	const signature = request.headers['btcpay-sig']
 
@@ -23,7 +23,7 @@ export const authenticateWebHook = (request, response, next) => {
 	}
 
 	console.log('this is the signature', signature)
-	console.log('this is the payloadBytes', payload)
+	console.log('this is the payload', payload)
 
 	next()
 
