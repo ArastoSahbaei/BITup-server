ab68e209d92e8adf29f7f3522d8074bd5c4f5534
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-17 22:16:55 +0100

    did this work?

--- src/middlewares/authenticateWebHook.ts
+++ src/middlewares/authenticateWebHook.ts
@@ -1,30 +1,25 @@
 import crypto from 'crypto'
 import StatusCode from '../configurations/StatusCode'
 
+const secret = 'a4YthnHjwYJ8qjEzgA2w7pouq1B' // The secret key provided by BTCPay Server
 export const authenticateWebHook = (request, response, next) => {
-	console.log('wtf is going on?')
-	console.log(request.body)
-	const secret = 'a4YthnHjwYJ8qjEzgA2w7pouq1B' // The secret key provided by BTCPay Server
-	// Extract the signature from the headers
-	const signature = request.headers['btcpay-sig']
 
-	// Extract the payload from the body
-	const payload = request.body
+	const payload = request.body // The webhook payload
+	const btcpaySig = request.headers['btcpay-sig'] // The BTCPAY-SIG header
+	const payloadBytes = JSON.stringify(payload) // Stringify payload to bytes
+	const btcpaySigWithoutAlgo = btcpaySig.split('=')[1] // remove the algorithm prefix
 
-	// Compute the HMAC256 of the payload
-	const hmac = crypto.createHmac('sha256', secret)
-	hmac.update(payload)
-	const computedSignature = hmac.digest('hex')
-
-	// Compare the signature from the headers with the computed signature
-	if (signature !== computedSignature) {
-		console.log('Invalid signature')
-		return request.status(401).send('Invalid signature')
-	}
-
-	console.log('this is the signature', signature)
 	console.log('this is the payload', payload)
-
+	console.log('this is the payloadBytes', payloadBytes)
+	const calculatedSig = crypto.createHmac('sha256', secret).update(payloadBytes).digest('hex')
+	console.log(calculatedSig + ' + ' + btcpaySigWithoutAlgo)
+	if (calculatedSig !== btcpaySigWithoutAlgo) {
+		console.log('UNAUTHORIZED')
+		return response.sendStatus(StatusCode.UNAUTHORIZED)
+	}
+	// The signature matches, the payload is valid
+	// Your code to handle the webhook payload goes here
+	// ...
 	next()
 
 }
