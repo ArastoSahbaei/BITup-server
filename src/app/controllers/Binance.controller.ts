f75347bf4e500c4bde249f6acd49c25189ac1a10
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-17 20:50:48 +0100

    ok we can test

--- src/app/controllers/Binance.controller.ts
+++ src/app/controllers/Binance.controller.ts
@@ -3,6 +3,7 @@ import StatusCode from '../../configurations/StatusCode'
 import BinanceService from '../../shared/api/services/BinanceService'
 import InvoiceModel from '../models/Invoice.model'
 import { invoiceStatus } from '../../shared/enums'
+import crypto from 'crypto'
 
 const testConnectivity = async (request, response) => {
 	try {
@@ -39,10 +40,11 @@ const createTrade = async (request, response) => {
 		const databaseResponse = await InvoiceModel.findOneAndUpdate({ BTCPAY_invoiceId: invoiceID }, { status: invoiceStatus.settled })
 
 		//3. Verify that the invoice has not already been used to create a trade (To ensure that the invoice is not used twice)
-		if (databaseResponse.status === invoiceStatus.settled) {
+		/* if (databaseResponse.status === invoiceStatus.settled) {
 			return response.status(StatusCode.METHOD_NOT_ALLOWED).send({ message: 'Invoice already settled' })
-		}
+		} */
 
+		//3.5 Verify that the quanntity is high enough to create a trade order
 		//4. Create a trade
 		//TODO: Why does invoiceResponse return an array? Shouldn't it return a single object? in what scenarios does it return multiple objects?
 		const invoiceResponse = await BTCPayService.getInvoicePaymentMethods(storeID, invoiceID)
@@ -66,8 +68,32 @@ const createTrade = async (request, response) => {
 	}
 }
 
+const test = (request, response) => {
+	try {
+		const payload = request.body // The webhook payload
+		console.log('this is the payload', payload)
+		const secret = 'yoursecretkey' // The secret key provided by BTCPay Server
+		const btcpaySig = request.headers['btcpay-sig'] // The BTCPAY-SIG header
+		console.log('this is the btcpaySig', btcpaySig)
+		const payloadBytes = JSON.stringify(payload) // Stringify payload to bytes
+		console.log('this is the payloadBytes', payloadBytes)
+		const calculatedSig = crypto.createHmac('sha256', secret).update(payloadBytes).digest('hex')
+		if (calculatedSig !== btcpaySig) {
+			// The signature does not match, the payload may have been tampered with or sent by an unauthorized source
+			return response.status(401).send('Unauthorized')
+		}
+		// The signature matches, the payload is valid
+		// Your code to handle the webhook payload goes here
+		// ...
+		response.send('Webhook received!')
+	} catch (error) {
+		console.log(error)
+	}
+}
+
 export default {
 	testConnectivity,
+	test,
 	getAccountInformation,
 	createTrade,
 }
--- src/app/routes/Binance.route.ts
+++ src/app/routes/Binance.route.ts
@@ -5,6 +5,7 @@ const routes = (application: Application) => {
 	application.get('/binance-test', BinanceController.testConnectivity)
 	application.get('/account', BinanceController.getAccountInformation)
 	application.post('/sell-order/store/:storeID/invoice/:invoiceID', BinanceController.createTrade)
+	application.post('/testhaha', BinanceController.test)
 }
 
 export default { routes }
