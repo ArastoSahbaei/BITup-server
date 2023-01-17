0c0178a64446db504d29cd80722020b7931e5401
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-17 22:36:24 +0100

    last

--- src/middlewares/authenticateWebHook.ts
+++ src/middlewares/authenticateWebHook.ts
@@ -9,10 +9,9 @@ export const authenticateWebHook = (request, response, next) => {
 	const payloadBytes = JSON.stringify(payload) // Stringify payload to bytes
 	const btcpaySigWithoutAlgo = signature.split('=')[1] // remove the algorithm prefix
 
-	console.log('this is the payload', payload)
-	console.log('this is the payloadBytes', payloadBytes)
 	const calculatedSig = crypto.createHmac('sha256', secret).update(payloadBytes).digest('hex')
 	console.log(calculatedSig + ' + ' + btcpaySigWithoutAlgo)
+	console.log(calculatedSig + ' + ' + signature)
 	if (calculatedSig !== btcpaySigWithoutAlgo) {
 		console.log('UNAUTHORIZED')
 		return response.sendStatus(StatusCode.UNAUTHORIZED)
