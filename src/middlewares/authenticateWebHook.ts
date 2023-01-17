7c4c342f9320c3e0a2942f01cc7210bc1e366449
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-17 22:31:12 +0100

    test

--- src/middlewares/authenticateWebHook.ts
+++ src/middlewares/authenticateWebHook.ts
@@ -6,18 +6,21 @@ export const authenticateWebHook = (request, response, next) => {
 
 	const payload = request.body
 	const signature = request.headers['btcpay-sig'] // The BTCPAY-SIG header
+	const payloadBytes = JSON.stringify(payload) // Stringify payload to bytes
 	const btcpaySigWithoutAlgo = signature.split('=')[1] // remove the algorithm prefix
-	const calculatedSig = crypto.createHmac('sha256', secret).update(payload).digest('hex')
+
+	console.log('this is the payload', payload)
+	console.log('this is the payloadBytes', payloadBytes)
+	const calculatedSig = crypto.createHmac('sha256', secret).update(payloadBytes).digest('hex')
 	console.log(calculatedSig + ' + ' + btcpaySigWithoutAlgo)
 	if (calculatedSig !== btcpaySigWithoutAlgo) {
-		console.log(calculatedSig === signature)
-		console.log(calculatedSig === btcpaySigWithoutAlgo)
 		console.log('UNAUTHORIZED')
 		return response.sendStatus(StatusCode.UNAUTHORIZED)
 	}
+	return response.sendStatus(StatusCode.OK)
 	// The signature matches, the payload is valid
 	// Your code to handle the webhook payload goes here
 	// ...
-	next()
+	/* 	next() */
 
 }
