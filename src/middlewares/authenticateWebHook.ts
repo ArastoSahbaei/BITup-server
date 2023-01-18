e62cb2ae2aac453a9052a3668092281a17d894d3
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-18 13:31:17 +0100

    how bout now

--- src/middlewares/authenticateWebHook.ts
+++ src/middlewares/authenticateWebHook.ts
@@ -2,8 +2,7 @@ import crypto from 'crypto'
 import StatusCode from '../configurations/StatusCode'
 
 export const authenticateWebHook = (request, response, next) => {
-	console.log(request.headers)
-	console.log('FUNCTION: authenticateWebHook')
+	console.log('request.headers', request.headers)
 
 	const secret = 'a4YthnHjwYJ8qjEzgA2w7pouq1B'
 	const payload = request.body
