7071ba76575437c79ee98da06f4f26f5429eab54
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-18 14:14:54 +0100

    m

--- src/middlewares/authenticateWebHook.ts
+++ src/middlewares/authenticateWebHook.ts
@@ -2,11 +2,11 @@ import crypto from 'crypto'
 import StatusCode from '../configurations/StatusCode'
 
 export const authenticateWebHook = (request, response, next) => {
-
-	const payload = JSON.stringify(request.body)
+	const payload = request.body
 	const secret = 'a4YthnHjwYJ8qjEzgA2w7pouq1B'
 	const signature = request.headers['btcpay-sig']
-	const hmac = crypto.createHmac('sha256', secret).update(payload).digest('hex')
+
+	const hmac = crypto.createHmac('sha256', secret).update(JSON.stringify(payload)).digest('hex')
 
 	if (hmac === signature) {
 		console.log('Payload is valid!')
