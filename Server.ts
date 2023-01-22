229d7c4d18a7fac152251d604b4f2ed3399bb4cd
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-22 12:05:55 +0100

    test

--- Server.ts
+++ Server.ts
@@ -2,20 +2,18 @@ import express from 'express'
 import { errorHandler, notFound } from './src/middlewares'
 import { connectToDatabase, connectToPort, useCors, useHelmet, useMorgan } from './src/functions'
 import { RouteHandler } from './src/app/routes/RouteHandler'
-import bodyParser from 'body-parser'
 
 const application = express()
-application.use(express.json())
-useCors(application)
-useHelmet(application)
-useMorgan(application)
-application.use(bodyParser.json({
+application.use(express.json({
 	verify: (req: any, res, buf) => {
 		console.log(buf.toString('utf8'))
 		console.log(buf)
 		req.rawBody = buf
 	}
 }))
+useCors(application)
+useHelmet(application)
+useMorgan(application)
 RouteHandler(application)
 application.use(notFound)
 application.use(errorHandler)
