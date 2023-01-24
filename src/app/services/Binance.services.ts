6822e7da2c6b413dffe83163afd8114b0f2da4aa
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-24 20:22:10 +0100

    now?

--- src/app/services/Binance.services.ts
+++ src/app/services/Binance.services.ts
@@ -4,9 +4,9 @@ import { invoiceStatus } from '../../shared/enums'
 
 export const verifyInvoice = async (storeId: string, invoiceId: string, response: any) => {
 	try {
+		return response.status(StatusCode.METHOD_NOT_ALLOWED).send({ message: 'Invoice not settled' })
 		const invoice = await BTCPayService.getInvoice(storeId, invoiceId)
 		const isInvoiceLegitimate: boolean = invoice.data.status === invoiceStatus.settled || invoice.data.status === invoiceStatus.inProcess
-		return response.status(StatusCode.METHOD_NOT_ALLOWED).send({ message: 'Invoice not settled' })
 		/* if (!isInvoiceLegitimate) {
     } */
 	} catch (error) {
