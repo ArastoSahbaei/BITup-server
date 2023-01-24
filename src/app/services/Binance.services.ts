c3cbf145988bff3149f6b48234c8ee941d714b40
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-01-24 20:50:30 +0100

    m

--- src/app/services/Binance.services.ts
+++ src/app/services/Binance.services.ts
@@ -6,13 +6,5 @@ export const verifyInvoice = async (storeId: string, invoiceId: string, response
 	console.log('verifyInvoice: ', storeId, invoiceId)
 	return response.status(StatusCode.METHOD_NOT_ALLOWED).send({ message: 'Invoice not settled' })
 
-	/* try {
-    const invoice = await BTCPayService.getInvoice(storeId, invoiceId)
-    const isInvoiceLegitimate: boolean = invoice.data.status === invoiceStatus.settled || invoice.data.status === invoiceStatus.inProcess
-    if (!isInvoiceLegitimate) {
-      return response.status(StatusCode.METHOD_NOT_ALLOWED).send({ message: 'Invoice not settled' })
-    }
-  } catch (error) {
-    console.log(error)
-  } */
+
 }
