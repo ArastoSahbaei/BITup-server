a5447b6b4546d1f1c26b9ec566eef6c98a01ffcb
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-03-05 22:02:17 +0100

    ok

--- src/functions/getWebhookSecret.ts
+++ src/functions/getWebhookSecret.ts
@@ -8,5 +8,6 @@ export const getWebhookSecret = () => {
 	//TODO: so apperently every store has its own webhook secret, so we need to get the secret from the store in the database and return it here
 	//1. Save in database? - Can anyone call the webhook if the secret is based on the store?
 
-	return isDevelopmentEnv() ? DEV_WEBHOOK_SECRET : PROD_WEBHOOK_SECRET
+	/* return isDevelopmentEnv() ? DEV_WEBHOOK_SECRET : PROD_WEBHOOK_SECRET */
+	return 'fz4QZQ7Wf95277Q5Co9CVqYjwrp'
 }
