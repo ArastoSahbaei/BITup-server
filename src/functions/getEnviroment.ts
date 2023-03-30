84dfac22a0f02a52cbfed40b092a94537ab66d95
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-03-30 13:08:20 +0200

    ok

--- src/functions/getEnviroment.ts
+++ src/functions/getEnviroment.ts
@@ -4,5 +4,7 @@ dotenv.config()
 const { ENVIROMENT } = process.env
 
 export const getEnviroment = () => {
+	console.log('getEnviroment')
+	console.log('ENVIROMENT', ENVIROMENT)
 	return ENVIROMENT
 }
