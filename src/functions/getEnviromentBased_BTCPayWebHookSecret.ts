e4c2da25eeafb5c88447bdd1b6ed24294a5fc81e
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-04-10 10:53:39 +0200

    removed comment

--- src/functions/getEnviromentBased_BTCPayWebHookSecret.ts
+++ src/functions/getEnviromentBased_BTCPayWebHookSecret.ts
@@ -5,8 +5,7 @@ import { enviromentOptions } from '../shared/constants'
 dotenv.config()
 const { DEV_WEBHOOK_SECRET, PROD_WEBHOOK_SECRET, STAGING_WEBHOOK_SECRET } = process.env
 
-//TODO: rename to match others
-export const getWebhookSecret = () => {
+export const getEnviromentBased_BTCPayWebHookSecret = () => {
 	switch (getEnviroment()) {
 	case enviromentOptions.development:
 		return DEV_WEBHOOK_SECRET
