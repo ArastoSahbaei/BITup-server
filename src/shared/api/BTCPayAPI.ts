612b237c97353fa90d13e4193d8da86005033554
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-04-13 21:14:04 +0200

    removed crap

--- src/shared/api/BTCPayAPI.ts
+++ src/shared/api/BTCPayAPI.ts
@@ -1,19 +1,11 @@
 import Axios from 'axios'
-import dotenv from 'dotenv'
-import { getEnviromentBased_BTCPayBaseURL, getEnviromentBased_BTCPayWebHookToken, isDevelopmentEnv } from '../../functions'
+import { getEnviromentBased_BTCPayBaseURL, getEnviromentBased_BTCPayWebHookToken } from '../../functions'
 
-/* dotenv.config()
-const { DEV_BTCPAY_TOKEN, PROD_BTCPAY_TOKEN } = process.env */
-
-/* const token = () => {
-	return isDevelopmentEnv() ? DEV_BTCPAY_TOKEN : PROD_BTCPAY_TOKEN
-} */
-
-const BitupAPI = Axios.create({
+const BTCPayAPI = Axios.create({
 	baseURL: getEnviromentBased_BTCPayBaseURL(),
 	headers: {
 		'Authorization': `token ${getEnviromentBased_BTCPayWebHookToken()}`
 	}
 })
 
-export default BitupAPI
+export default BTCPayAPI
