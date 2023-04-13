b2a9ca687fa7c61fbc77d7c15a711dc48db9d8d5
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-04-13 21:40:02 +0200

    ok

--- src/shared/api/BinanceAPI.ts
+++ src/shared/api/BinanceAPI.ts
@@ -1,4 +1,36 @@
 import Axios from 'axios'
+import dotenv from 'dotenv'
+import { isDevelopmentEnv } from '../../functions'
+
+dotenv.config()
+const { DEV_BINANCE_TOKEN, PROD_BINANCE_TOKEN } = process.env
+
+const baseURL = () => {
+	const baseURL_PROD = 'https://api.binance.com'
+	const baseURL_DEV = 'https://testnet.binance.vision'
+	return isDevelopmentEnv() ? baseURL_DEV : baseURL_PROD
+}
+
+const getBinanceSecretToken = () => {
+	return isDevelopmentEnv() ? DEV_BINANCE_TOKEN : PROD_BINANCE_TOKEN
+}
+
+const headers = {
+	'Content-Type': 'application/json',
+	'X-MBX-APIKEY': getBinanceSecretToken()
+}
+
+const BinanceAPI = Axios.create({
+	baseURL: baseURL(),
+	headers: headers
+})
+
+
+export default BinanceAPI
+
+
+
+/* import Axios from 'axios'
 import { getEnviromentBased_binanceBaseURL, getEnviromentBased_binanceSecret } from '../../functions'
 
 const headers = {
@@ -11,4 +43,4 @@ const BinanceAPI = Axios.create({
 	headers: headers
 })
 
-export default BinanceAPI
+export default BinanceAPI */
