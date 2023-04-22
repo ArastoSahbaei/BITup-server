4a8784de2f447007777886ae8a6f85965ebf6969
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-04-22 21:33:16 +0200

    added endpoint path

---
+++ src/app/routes/endpoints.ts
@@ -0,0 +1,25 @@
+export const endpoints = {
+	authentication: {
+		login: '/login',
+		createUser: '/register',
+		verifyUserEmail: '/emailverification',
+		retrieveLostAccount: '/retrieveaccount',
+		resetPasswordWithToken: '/resetpassword',
+		validateToken: '/validate',
+		updatePassword: '/update-password'
+	},
+	user: {
+		getAllUser: '/user',
+	},
+	binance: {
+		testConnectivity: '/binance-test',
+		getAccountInformation: '/account',
+		createTrade: '/sell-order',
+	},
+	BTCPay: {
+		getInvoices: '/btcpay/invoices',
+		createInvoice: '/btcpay/invoices/create',
+		getInvoice: '/btcpay/invoices/:storeID/:invoiceID',
+		getInvoicePaymentMethods: '/btcpay/store/:storeID/invoices/:invoiceID/payment-methods',
+	}
+}
