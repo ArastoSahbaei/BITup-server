d9fa4628e1f9222d5df044a34e75b2bd0fe3ac98
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-05-07 11:00:36 +0200

    ok

--- src/functions/useMorgan.ts
+++ src/functions/useMorgan.ts
@@ -1,5 +1,5 @@
 
-import { enviromentOptions } from 'src/shared/constants'
+import { enviromentOptions } from 'src/shared/constants/enviromentOptions'
 import { Application } from 'express'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
